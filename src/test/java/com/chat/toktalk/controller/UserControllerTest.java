f3ee82848e6ba3e515f97e4af5b9313ca3bb8d74
serena <noriming2@gmail.com>
2018-09-29 03:01:53 +0900

    update : 로그인 테스트 추가

--- src/test/java/com/chat/toktalk/controller/UserControllerTest.java
+++ src/test/java/com/chat/toktalk/controller/UserControllerTest.java
@@ -10,12 +10,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -28,9 +25,34 @@ public class UserControllerTest {
     @Test
     public void 로그인성공테스트() throws Exception {
         this.mockMvc
-                .perform(get("/users/login")
-                        .param("test@gmail.com","1234"))
-                .andExpect(model().hasNoErrors())
-                .andExpect(status().is2xxSuccessful());
+                .perform(post("/users/login")
+                        .param("email", "noriming2@gmail.com")
+                        .param("password", "1234"))
+                .andDo(print())
+                .andExpect(cookie().exists("SESSION"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void 로그인실패테스트() throws Exception {
+        this.mockMvc
+                .perform(post("/users/login")
+                        .param("email", "noriming2@gmail.com")
+                        .param("password", "1111"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/login?error"));
+    }
+
+    @Test
+    public void 리멤버미로그인테스트() throws Exception {
+        this.mockMvc
+                .perform(post("/users/login")
+                .param("email","noriming2@gmail.com")
+                .param("password","1234")
+                .param("remember-me","true"))
+                .andDo(print())
+        .andExpect(cookie().exists("remember-me"));
     }
 }
