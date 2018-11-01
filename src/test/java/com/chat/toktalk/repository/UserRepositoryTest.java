5075be28c554336d6703459c1c57197004c95ca7
serena <noriming2@gmail.com>
2018-11-01 13:27:47 +0900

    delete OauthRepository test

--- src/test/java/com/chat/toktalk/repository/UserRepositoryTest.java
+++ src/test/java/com/chat/toktalk/repository/UserRepositoryTest.java
@@ -41,26 +41,4 @@ public class UserRepositoryTest {
         assertEquals(result.getNickname(),"뚜루뚜루");
     }
 
-    @Test
-    public void 이메일로_구글로그인정보조회하기(){
-        
-        User registerUser = new User();
-        registerUser.setEmail("아기상어@gmail.com");
-        registerUser.setPassword(passwordEncoder.encode("1234"));
-        registerUser.setNickname("뚜루뚜루");
-
-        OauthInfo oauthInfo = new OauthInfo();
-        oauthInfo.setEmail("아기상어@gmail.com");
-
-        registerUser.addUserOauthInfo(oauthInfo);
-        userRepository.save(registerUser);
-
-        User result = userRepository.findOauthUserByEmail("아기상어@gmail.com");
-
-        assertEquals(registerUser.getOauthInfos().size(),1);
-        assertEquals(result.getNickname(),"뚜루뚜루");
-
-    }
-
-
 }
