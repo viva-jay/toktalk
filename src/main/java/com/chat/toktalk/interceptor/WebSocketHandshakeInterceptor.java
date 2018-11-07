643796917ad2564f7d9d2e31fa78ec7f91ecbf97
serena <noriming2@gmail.com>
2018-11-07 23:14:28 +0900

    update Authentication 가져오는 방식 변경

--- src/main/java/com/chat/toktalk/interceptor/WebSocketHandshakeInterceptor.java
+++ src/main/java/com/chat/toktalk/interceptor/WebSocketHandshakeInterceptor.java
@@ -3,33 +3,27 @@ package com.chat.toktalk.interceptor;
 import com.chat.toktalk.security.LoginUserInfo;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.Map;
 
+@Component
 public class WebSocketHandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-        ServletServerHttpRequest servletServerHttpRequest = (ServletServerHttpRequest) request;
-        HttpServletRequest req = servletServerHttpRequest.getServletRequest();
 
-        HttpSession session = req.getSession();
-        SecurityContext securityContext = (SecurityContext) session.getAttribute("SPRING_SECURITY_CONTEXT");
-        Authentication authentication = securityContext.getAuthentication();
+        Authentication authentication = (Authentication) request.getPrincipal();
 
-        if(authentication != null && authentication.getPrincipal() instanceof LoginUserInfo){
-            LoginUserInfo loginUserInfo = (LoginUserInfo)authentication.getPrincipal();
+        if (authentication != null && authentication.getPrincipal() instanceof LoginUserInfo) {
+            LoginUserInfo loginUserInfo = (LoginUserInfo) authentication.getPrincipal();
             attributes.put("userId", loginUserInfo.getId());
             attributes.put("nickname", loginUserInfo.getNickname());
             attributes.put("username", loginUserInfo.getEmail());
         }
-
         return true;
     }
 
